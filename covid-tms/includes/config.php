82e7b0208c47c665531b3d125c107811a18a4434
shristi1106 <shristiverma1311@gmail.com>
2022-04-10 19:06:56 +0530

    made some changes

--- covid-tms/includes/config.php
+++ covid-tms/includes/config.php
@@ -2,6 +2,7 @@
 //time zone - Indian
 date_default_timezone_set('Asia/Kolkata');
 //database connection
+//  data base name - covidtmsdb
 $con=mysqli_connect("localhost","root","","covidtmsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
