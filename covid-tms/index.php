de8134ae16b552038714b43171559f3569102cf5
rahulsinghbisht-13 <rbisht733@gmail.com>
2022-04-11 10:38:15 +0530

    final commit

--- covid-tms/index.php
+++ covid-tms/index.php
@@ -113,7 +113,7 @@ Reported illnesses have ranged from mild symptoms to severe illness and death</p
   <!-- Footer -->
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+      <p class="m-0 text-center text-white">Copyright &copy; Rahul Singh Bisht 2022</p>
     </div>
     <!-- /.container -->
   </footer>
